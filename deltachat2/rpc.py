91afe9c0518f0f5fb719e4a675a89db2e709272f
adbenitez <asieldbenitez@gmail.com>
2024-05-20 22:48:15 +0200

    improve self-message detection in communities

--- deltachat2/rpc.py
+++ deltachat2/rpc.py
@@ -19,4 +19,6 @@ class Rpc:
     def send_msg(self, accid: int, chatid: int, data: MsgData) -> int:
         """Send a message and return the message ID of the sent message."""
         json_obj = _snake2camel(dataclasses.asdict(data))
-        return self.transport.call("send_msg", accid, chatid, json_obj)
+        msgid = self.transport.call("send_msg", accid, chatid, json_obj)
+        self.markseen_msgs(accid, [msgid])
+        return msgid
